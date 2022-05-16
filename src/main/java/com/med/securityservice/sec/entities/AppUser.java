00331204881abd26205ea7f36c184dd53e9e9c5c
Medkaffouh <kaffouhmohamed@gmail.com>
2022-05-16 18:29:30 +0100

    Initial commit

---
+++ src/main/java/com/med/securityservice/sec/entities/AppUser.java
@@ -0,0 +1,18 @@
+package com.med.securityservice.sec.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Collection;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class AppUser {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    @ManyToMany
+    private Collection<AppRole> appRoles;
+}
---
+++ src/main/java/com/med/securityservice/sec/repositories/AppUserRepository.java
@@ -0,0 +1,4 @@
+package com.med.securityservice.sec.repositories;
+
+public interface AppUserRepository {
+}
